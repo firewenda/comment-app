e80fae57cd3acc6ac5604afdec72a17ccec5ae33
litong <lit850@joyowo.com>
2018-02-24 16:02:23 +0800

    update

--- src/CommentApp.js
+++ src/CommentApp.js
@@ -1,23 +1,39 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import CommentInput from './CommentInput';
 import CommentList from './CommentList';
+import wrapWithLoadData from './wrapWithLoadData';
 
 class CommentApp extends Component{
-    constructor(){
-        super();
+    static propTypes = {
+        data: PropTypes.any,
+        saveData: PropTypes.func.isRequired
+    }
+
+    constructor(props){
+        super(props);
         this.state = {
-            comments: []
+            comments: props.data
         }
     }
 
-    handleSubmitComment(comment){
-        if(!comment || !comment.username || !comment.content) return;
+    handleSubmitComment (comment) {
+        if (!comment) return
+        if (!comment.username) return alert('请输入用户名');
+        if (!comment.content) return alert('请输入评论内容');
+        const comments = this.state.comments
+        comments.push(comment)
+        this.setState({ comments })
+        this.props.saveData(comments)
+    }
 
-        this.state.comments.push(comment);
-        this.setState({
-            comments: this.state.comments
-        })
-        //console.log(comment);
+    handleDeleteComment(index){
+        const comments = this.state.comments;
+        comments.splice(index, 1);
+        this.setState({comments});
+        this.props.saveData(comments);
+        console.log(index);
     }
 
     render(){
@@ -25,10 +41,14 @@ class CommentApp extends Component{
             <div className='wrapper'>
                 <CommentInput
                 onSubmit={this.handleSubmitComment.bind(this)} />
-                <CommentList comments={this.state.comments} />
+                <CommentList 
+                comments={this.state.comments}
+                onDeleteComment={this.handleDeleteComment.bind(this)} />
             </div>
         )
     }
 }
+
+CommentApp = wrapWithLoadData(CommentApp, 'comments')
 
 export default CommentApp;
--- src/CommentInput.js
+++ src/CommentInput.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import wrapWithLoadData from './wrapWithLoadData';
 
 class CommentInput extends Component{
-    constructor(){
-        super();
+    static propTypes = {
+        onSubmit: PropTypes.func,
+        data: PropTypes.any,
+        saveData: PropTypes.func.isRequired
+    }
+
+    constructor(props){
+        super(props);
         this.state = {
-            username: '',
+            username: props.data,
             content: ''
         }
+    }
+
+    componentDidMount(){
+        this.textarea.focus();
     }
 
     handleUsernameChange(event){
         this.setState({
             username: event.target.value
         })
+    }
+
+    handleUsernameBlur(event){
+        this.props.saveData(event.target.value)
     }
 
     handleContentChange(event){
@@ -23,8 +40,11 @@ class CommentInput extends Component{
 
     handleSubmit(){
         if(this.props.onSubmit){
-            const { username, content } = this.state;
-            this.props.onSubmit({username, content})
+            this.props.onSubmit({
+                username: this.state.username,
+                content: this.state.content,
+                createdTime: +new Date()
+            })
         }
         this.setState({ content: '' })
     }
@@ -37,6 +57,7 @@ class CommentInput extends Component{
                     <div className='comment-field-input'>
                         <input
                          value={this.state.username}
+                         onBlur={this.handleUsernameBlur.bind(this)}
                          onChange={this.handleUsernameChange.bind(this)} />
                     </div>
                 </div>
@@ -44,6 +65,7 @@ class CommentInput extends Component{
                     <span className='comment-field-name'>评论内容：</span>
                     <div className='comment-field-input'>
                         <textarea 
+                        ref={(textarea) => this.textarea = textarea}
                         value={this.state.content}
                         onChange={this.handleContentChange.bind(this)} />
                     </div>
@@ -57,5 +79,7 @@ class CommentInput extends Component{
         )
     }
 }
+
+CommentInput = wrapWithLoadData(CommentInput, 'username');
 
 export default CommentInput;
